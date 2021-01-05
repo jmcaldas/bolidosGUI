fbb00e61698b24c68fb2235c986f4ea9c4aff7a3
Manuel Caldas <74320205+jmcaldas@users.noreply.github.com>
2021-01-05 10:00:06 -0300

    Delete polar2ij.m

--- polar2ij.m
+++
@@ -1,8 +0,0 @@
-function IJ = polar2ij(rho,theta,ic,jc)
-%Pasa de coordenadas polares en un sistema
-%de origen en el cenit (xc,yc), con y hacia arriba y x a la derecha, a pixel (i,j).
-
-IJ(:,2) = jc+rho.*cosd(theta);
-IJ(:,1) = ic-rho.*sind(theta);
-
-end
