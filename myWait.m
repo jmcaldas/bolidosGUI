554fbcc62d0b1366acb2bbb706bb83735cad20d7
Manuel Caldas <74320205+jmcaldas@users.noreply.github.com>
2021-01-05 09:59:25 -0300

    Delete myWait.m

--- myWait.m
+++
@@ -1,7 +0,0 @@
-% Waits for the specified number of seconds
-function myWait(DeltaT)
-if(DeltaT>0) %end condition
-    t=timer('timerfcn','myWait(0)','StartDelay',DeltaT);
-    start(t);
-    wait(t);
-end
